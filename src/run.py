3faf7b0bc50c3f604a81891ecdb161e43f624baf
Kavinda Rajapakse <kavindsr@gmail.com>
2023-05-22 23:41:01 +0530

    Create dir if not exists

--- src/run.py
+++ src/run.py
@@ -1,6 +1,14 @@
 import sys
+import os
 import data_extration as de
 import train as model
+
+# checking if the directory
+# exist or not.
+if not os.path.exists("out"):
+    os.makedirs("out")
+if not os.path.exists("Data/processed_data"):
+    os.makedirs("Data/processed_data")
 
 if len(sys.argv) < 4:
     print("Usage: python3 run.py <positive training data> <negative training data> <positive testing data> <negative testing data>")
